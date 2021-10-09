5355232192764d5e0c4b0aed4232282376493a33
Richard Cranwell <rickycranwell@gmail.com>
2021-10-09 15:44:20 -0500

    move dropdown to a module file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-//test that webpack works
-console.log('webpack compiled to main.js');
+import dropdownFactory from './dropdown';
+
+const holder = document.querySelector('#dropdown');
+
+const sampleTitle = document.createElement('button');
+sampleTitle.innerText = 'I am a button';
+
+const button1 = document.createElement('button');
+button1.innerText = 'I am a button 2';
+
+const button2 = document.createElement('button');
+button2.innerText = 'I am a button 3';
+
+const dropdown = dropdownFactory(sampleTitle, [button1, button2]);
+dropdown.attachTo(dropdown, holder);
