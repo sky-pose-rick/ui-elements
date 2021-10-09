a6ff8b89eceb9df30593abb13cc2366cbddf9872
Richard Cranwell <rickycranwell@gmail.com>
2021-10-09 15:47:56 -0500

    fix import for dropdown

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import dropdownFactory from './dropdown';
+import dropdown from './dropdown';
 
-const holder = document.querySelector('#dropdown');
+const ddHolder = document.querySelector('#dropdown');
 
-const sampleTitle = document.createElement('button');
-sampleTitle.innerText = 'I am a button';
+const ddSampleTitle = document.createElement('button');
+ddSampleTitle.innerText = 'I am a button';
 
-const button1 = document.createElement('button');
-button1.innerText = 'I am a button 2';
+const ddButton1 = document.createElement('button');
+ddButton1.innerText = 'I am a button 2';
 
-const button2 = document.createElement('button');
-button2.innerText = 'I am a button 3';
+const ddButton2 = document.createElement('button');
+ddButton2.innerText = 'I am a button 3';
 
-const dropdown = dropdownFactory(sampleTitle, [button1, button2]);
-dropdown.attachTo(dropdown, holder);
+const dropdownElem = dropdown.dropdownFactory(ddSampleTitle, [ddButton1, ddButton2]);
+dropdownElem.attachTo(dropdownElem, ddHolder);
