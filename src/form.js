5ff05c7ca9de48f7dee56af11fd562c7dadc91af
Richard Cranwell <rickycranwell@gmail.com>
2021-10-11 14:42:52 -0500

    add an input creation function

--- src/form.js
+++ src/form.js
@@ -1,6 +1,24 @@
 //  create a form that validates live
 //  able to add fields to the form
 
+const inputFactory = (name, validation, required) => {
+  const label = document.createElement('label');
+  const input = label.createElement('input');
+  label.appendChild(input);
+
+  if (required) {
+    input.toggleAttribute('required');
+  }
+
+  input.addEventListener('blur', () => {
+    validation();
+  });
+
+  //  do stuff
+  const obj = { label, input };
+  return obj;
+};
+
 const formFactory = () => {
   //  do stuff
   const obj = { stub: 'stub' };
